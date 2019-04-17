a17fdaad3562211ade0ddcd5bb167ccb6b4eede8
Gabidal <41587318+Gabidal@users.noreply.github.com>
2019-04-17 15:32:22 +0300

    typo

--- Gas/Prism/GIC.asm
+++ Gas/Prism/GIC.asm
@@ -13,7 +13,7 @@ func 1, '+', 0, 0, 1 ;get the return of the "8_pass".
 ;FUNC 8_pass()
 ;{
 func 1, '*', 0, 1, 2 ; the result vill divide 5
-func 1, '+', 0, 1, 2 ;gey the return 7 and +1 and then pass it to next.
+func 1, '+', 0, 1, 2 ;get the return 7 and +1 and then pass it to next.
 func 5, '+', 2, 1, 0 ;return 5+2.
 ;}
 
